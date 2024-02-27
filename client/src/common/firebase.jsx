8f0a6a2cca629a5526bdfe04db8abc2b6f65e80a
Rohit Raj <singh.rohitraj122@gmail.com>
2024-02-27 18:36:04 +0530

    google login added

---
+++ client/src/common/firebase.jsx
@@ -0,0 +1,28 @@
+import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
+
+export const signInWithGoogle = async () => {
+  let user = null;
+  await signInWithPopup(auth, provider)
+    .then((result) => {
+      user = result.user;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return user;
+};
--- server/app.js
+++ server/app.js
@@ -1,15 +1,24 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import admin from 'firebase-admin';
+import serviceAccount from './FIREBASE_ADMIN_SDK.json' assert { type: 'json' };
 import { register } from './Controllers/Register.js';
 import { login } from './Controllers/Login.js';
-import connectDB from './utils/dbconn.js';
+import connectDB from './Utils/dbconn.js';
+import { googleAuth } from './Controllers/GoogleAuth.js';
 const app = express();
 let PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(cors());
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 app.post('/signup', register);
 app.post('/signin', login);
+app.post('/google-auth', googleAuth);
 
 app.listen(PORT, async () => {
   await connectDB();
