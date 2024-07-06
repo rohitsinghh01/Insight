2b40126c3327a4c908fd139248dc425a98b50963
Rohit Raj <singh.rohitraj122@gmail.com>
2024-07-06 23:29:32 +0530

    trending blog implemented

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,38 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import PageAnimation from '../common/PageAnimation';
-import InPageNavigaion, {
-  activeTabRef,
-} from '../components/Inpage-navigation';
+import InPageNavigaion, { activeTabRef } from '../components/Inpage-navigation';
 import BlogPostCard from '../components/BlogPostCard';
-import Loader from '../components/Loader'
+import MinimalBlogPostCard from '../components/MinimalBlogPostCard';
+import Loader from '../components/Loader';
 import axios from 'axios';
 
 const Home = () => {
+  let [pageState, setPageState] = useState('home');
+  let [blogs, setBlogs] = useState(null);
+  let [trendingBlogs, setTrendingBlogs] = useState(null);
+  
+  
+  const fetchLatestBlogs = ({ page = 1 }) => {
+    axios
+      .get(import.meta.env.VITE_FRONTEND_URL + '/latest-blog', { page })
+      .then(async ({ data }) => {
+        // console.log(data.blogs)
 
-    let [pageState, setPageState] = useState('home')
-      let [blogs, setBlogs] = useState(null);
-      const fetchLatestBlogs = ({ page = 1 }) => {
-        axios
-          .get(import.meta.env.VITE_FRONTEND_URL + '/latest-blog', { page })
-          .then(async ({ data }) => {
-            // console.log(data.blogs)
-      
-            // console.log(formateData)
-            setBlogs(data.blogs);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      };
+        // console.log(formateData)
+        setBlogs(data.blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-       useEffect(() => {
-         activeTabRef.current.click();
-         if (pageState === 'home') fetchLatestBlogs({ page: 1 });
-         else {
-          //  fetchBlogByCategory({ page: 1 });
-         }
-        //  if (!trendingBlogs) fetchTrendingBlogs();
-       }, [pageState]);
+
+
+  const fetchTrendingBlogs = () => {
+    axios
+      .get(import.meta.env.VITE_FRONTEND_URL + '/trending-blog')
+      .then(({ data }) => {
+        // console.log(data.blogs);
+        setTrendingBlogs(data.blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+
+
+  useEffect(() => {
+    activeTabRef.current.click();
+    if (pageState === 'home') fetchLatestBlogs({ page: 1 });
+    else {
+      //  fetchBlogByCategory({ page: 1 });
+    }
+    if (!trendingBlogs) fetchTrendingBlogs();
+  }, [pageState]);
+
+
+
   return (
     <PageAnimation>
       <section className='h-cover flex justify-center gap-10'>
@@ -45,7 +65,7 @@ const Home = () => {
             <>
               {blogs === null ? (
                 <Loader />
-              ): (
+              ) : (
                 blogs.map((blog, i) => {
                   return (
                     <PageAnimation
@@ -56,6 +76,22 @@ const Home = () => {
                         content={blog}
                         author={blog.author.personal_info}
                       />
+                    </PageAnimation>
+                  );
+                })
+              )}
+            </>
+            <>
+              {trendingBlogs === null ? (
+                <Loader />
+              ) : (
+                trendingBlogs.map((blog, i) => {
+                  return (
+                    <PageAnimation
+                      key={i}
+                      transition={{ duration: 1, delay: i * 0.1 }}
+                    >
+                      <MinimalBlogPostCard blog={blog} index={i} />
                     </PageAnimation>
                   );
                 })
